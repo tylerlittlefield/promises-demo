a94803d13bbfee7e32ea5f47d2818aa1bb770330
tylurp <tylurp1@gmail.com>
2019-11-28 08:24:54 -0800

    test

--- server.R
+++ server.R
@@ -1,10 +1,27 @@
 server <- function(input, output, session) {
+  
+  rv <- reactiveValues(large_data = data.frame())
+  
   observeEvent(input$go, {
     
-    switch (input$data,
-      "5 Seconds" = future({ wait_n(5) }),
-      "10 Seconds" = future({ wait_n(10) })
-    )
+    if (input$data == "5 seconds") {
+      
+      future({
+        wait_n(5)
+        data.frame("wait" = 5)
+      }) %...>%
+        assign(rv[["large_data"]], .)
+      
+    } else if (input$date == "10 seconds") {
+      
+      future({
+        wait_n(10)
+        data.frame("wait" = 10)
+      }) %...>%
+        assign(rv[["large_data"]], .)
+      
+    }
+    
     
     hide_waiter()
     
