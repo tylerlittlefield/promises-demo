d42c008784296ae22fc93f6fbe00dad4cb5f07e6
tylurp <tylurp1@gmail.com>
2019-11-28 08:35:47 -0800

    fix

--- server.R
+++ server.R
@@ -6,24 +6,25 @@ server <- function(input, output, session) {
     
     if (input$data == "5 Seconds") {
       
+      wait_n(5)
+      
       future({
-        wait_n(5)
+        hide_waiter()
         data.frame("wait" = 5)
       }) %...>%
         assign(rv[["large_data"]], .)
       
     } else if (input$data == "10 Seconds") {
       
+      wait_n(10)
+      
       future({
-        wait_n(10)
+        hide_waiter()
         data.frame("wait" = 10)
       }) %...>%
         assign(rv[["large_data"]], .)
       
     }
-    
-    
-    hide_waiter()
     
   })
 }
