0ba09c119a87125b305f9bf17ca04e4d2b1bdddf
tylurp <tylurp1@gmail.com>
2019-11-28 08:50:10 -0800

    more testing

--- server.R
+++ server.R
@@ -6,23 +6,13 @@ server <- function(input, output, session) {
     
     if (input$data == "5 Seconds") {
       
-      wait_n(5)
-      
-      future({
-        hide_waiter()
-        data.frame("wait" = 5)
-      }) %...>%
-        assign(rv[["large_data"]], .)
+      future({ wait_n(5) }) %...>%
+        hide_water()
       
     } else if (input$data == "10 Seconds") {
       
-      wait_n(10)
-      
-      future({
-        hide_waiter()
-        data.frame("wait" = 10)
-      }) %...>%
-        assign(rv[["large_data"]], .)
+      future({ wait_n(10) }) %...>%
+        hide_water()
       
     }
     
